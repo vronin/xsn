4760e9eab9e16d13110c69248d69ca6b1d53e29c
Victor Ronin <victor.ronin@gmail.com>
2013-08-28 16:41:39 -0400

    Small refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Xsn::Application.routes.draw do
-#  resources :events
-#  resources :ideas
-
   get "welcome/index"
 
   devise_for :users
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
