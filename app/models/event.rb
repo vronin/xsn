d91110ba2ad092b1aa9418eaedc1d57201ac0ff7
Victor Ronin <victor.ronin@gmail.com>
2013-08-28 16:18:01 -0400

    Added validation of fields

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :idea, :inverse_of => :event
   
+  validates :location, presence: true  
+  validates :idea, presence: true
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,6 @@
 class Idea < ActiveRecord::Base
   has_many :event, :inverse_of => :idea
   
-  validates :title, presence: true
-  validates :description, presence: true
-  
+  validates :title, presence: true  
+  validates :description, presence: true  
 end
