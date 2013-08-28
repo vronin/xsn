d67f0bce33c0940d7652fac5b1cd1575074276c1
Victor Ronin <victor.ronin@gmail.com>
2013-08-27 21:54:14 -0400

    Added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Xsn::Application.routes.draw do
+  get "welcome/index"
   resources :ideas
 
   devise_for :users
@@ -7,7 +8,7 @@ Xsn::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
